df413b1ae7dd5b905b876365c00d75ac75ef995d
yen kuang chuang <5915590+antigenius0910@users.noreply.github.com>
2021-02-15 12:20:30 -0600

    add hack/ci

---
+++ hack/ci/run-unit-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+   
+    build_root=$PWD
+    echo "Build Root: ${build_root}"
+
+    cd workspace-repo
+    ls -al
+    echo $PATH
+
+    apt update
+    apt install -y libssl-dev
+    apt install -y libpcre3-dev
+    apt install -y automake 
+
+    ./bootstrap.sh
+    builddate=$(date --utc +%Y-%m-%d)
+    ./configure --enable-agent --with-openssl --program-prefix=cld- --program-suffix=-$builddate --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc
+
+    make
+    make test COVERAGE_DIR=${build_root}/coverage
