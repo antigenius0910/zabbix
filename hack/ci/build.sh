edb07232106d505a77ab4462363f00204bf4cbe6
yen kuang chuang <5915590+antigenius0910@users.noreply.github.com>
2021-02-17 13:35:08 -0600

    add build.sh

---
+++ hack/ci/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+   
+    build_root=$PWD
+    echo "Build Root: ${build_root}"
+
+    cd workspace-repo
+
+    apt update
+    apt install -y libssl-dev
+    apt install -y libpcre3-dev
+    apt install -y automake 
+    apt install -y checkinstall
+
+    ./bootstrap.sh
+    builddate=$(date --utc +%Y-%m-%d)
+
+    ./configure \
+            --bindir=/usr/bin \
+            --sbindir=/usr/sbin \
+            --datadir=/usr/lib \
+            --libdir=/usr/lib/zabbix \
+            --sysconfdir=/etc/zabbix \
+            --program-prefix=cld- \
+            --program-suffix=-$builddate \
+            --with-openssl \
+            --enable-agent 
+
+    make
+    checkinstall --install=no --default 
